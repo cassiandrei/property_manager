d04516f8fe58840c6e22372db5c102da9dd733e2
Cassiano Andrei <cschneider@inf.ufsm.br>
2021-03-20 19:39:06 -0300

    Docker concluído

---
+++ update.sh
@@ -0,0 +1,4 @@
+sudo cp -r api/* /var/lib/docker/volumes/property_manager_api_data/_data
+#docker exec -i -t api-delivery pip install -r requirements.txt
+#docker exec -i -t api-delivery python manage.py makemigrations
+#docker exec -i -t api-delivery python manage.py migrate
