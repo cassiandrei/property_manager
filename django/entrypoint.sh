0449c2d4265320fe0b74e4bcb7a2a0e55bbdd333
Cassiano Andrei <cschneider@inf.ufsm.br>
2021-03-20 19:21:01 -0300

    Docker concluído

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -3,7 +3,7 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --no-input
-python3 manage.py shell < populate.py
+python3 manage.py shell < populator.py
 #cp -r bkp/media/ .
 
 exec "$@"
