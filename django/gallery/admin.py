e5b182e9460c5dbff538559607ad2f8e3aa96d74
Cassiano Andrei <cschneider@inf.ufsm.br>
2021-03-20 19:28:59 -0300

    Docker concluído

--- django/gallery/admin.py
+++ django/gallery/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from gallery.models import Foto
 
-admin.site.register(Foto)
+
+class HiddenAdmin(admin.ModelAdmin):
+    def has_module_permission(self, request):
+        return False
+
+
+admin.site.register(Foto, HiddenAdmin)
