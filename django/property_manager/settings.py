731d571ebaee136dc3e269440375721fb816e76c
Cassiano Andrei <cschneider@inf.ufsm.br>
2021-03-20 17:39:35 -0300

    gitignore

--- django/property_manager/settings.py
+++ django/property_manager/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = os.path.join(BASE_DIR, 'august-tract-298612-ca1946603460.json')
 GOOGLE_DRIVE_STORAGE_MEDIA_ROOT = 'imoveis' # OPTIONAL
