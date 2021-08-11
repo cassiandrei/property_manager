b8ef7da9c12b0121e177da14dc12f3f827a61044
cassiano <cschneider@inf.ufsm.br>
2021-08-11 09:25:17 -0300

    removendo classe todos imoveis

--- django/properties/admin.py
+++ django/properties/admin.py
@@ -239,8 +239,5 @@ class ImovelAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-
 
 admin.site.register(Imovel, ImovelAdmin)
